12e7eb2eacc3f7a94ca8d6c38e0818a2b3d691ad
Andrea Ramirez <andrea.anza3@gmail.com>
2022-12-10 15:08:54 -0600

    Added a prompt that will ask the user to input the desired password length, and a conditional that will check if the desired length is at least 8 characters long and no more than 128

--- script.js
+++ script.js
@@ -13,9 +13,43 @@ var numerical = ["0123456789"]; console.log (numerical) ;
 var specialCharacters = ["!#$%&'()*+,-./:;<=>?@[^_`{|}~"]; console.log (specialCharacters);
 
 
+// GeneratePassword function starts here
 
-// Write password to the #password input
-function writePassword() {
+
+function generatePassword() {
+
+
+// In this prompt the password length is determined by the user
+
+
+var userLength = parseInt (window.prompt ("Type the length of your password. Min 8 characters. Max 128 characters"));
+
+
+// In this section if the password is outside of the parameters, an alert will pop and then it will take you back to the first length prompt
+
+
+if ( userLength >= 8 && userLength <= 128){
+  console.log("we good");
+} else {
+  alert("Your password length must be from 8 to 128 characters");
+  var userLength = parseInt (window.prompt ("Type the length of your password. Min 8 characters. Max 128 characters"));
+}
+console.log(userLength);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
