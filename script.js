a746b90ae2abee69cd6aa11e489ffe6b47a42109
Andrea Ramirez <andrea.anza3@gmail.com>
2022-12-10 14:59:20 -0600

    Added arrays storing all the different character groups

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 // Assignment Code
+
+
 var generateBtn = document.querySelector("#generate");
+
+
+// These arrays store all the different char groups that can be used for the password
+
+
+var lowerCase = ["abcdefghijklmnopqrsuvwxyz"]; console.log(lowerCase) ;
+var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]; console.log(upperCase) ;
+var numerical = ["0123456789"]; console.log (numerical) ;
+var specialCharacters = ["!#$%&'()*+,-./:;<=>?@[^_`{|}~"]; console.log (specialCharacters);
+
+
 
 // Write password to the #password input
 function writePassword() {
